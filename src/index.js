9caff1de913ef9b17ec0a7989ee146a5026a67ac
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2020-01-09 01:38:30 +0530

    dom elements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+function createElement (type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === 'object' ? child : createTextElement(child)
+      )
+    }
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function createTextElement (text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function render (element, container) {
+  const dom =
+    element.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(element.type)
+  const isProperty = key => key !== 'children';
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    })
+  element.props.children.forEach(child => render(child, dom))
+  container.appendChild(dom)
+}
+
+const Didact = {
+  createElement,
+  render
+}
+
+/** @jsx Didact.createElement */
+const element = (
+  <div style='background: salmon'>
+    <h1>Hello World</h1>
+    <h2 style='text-align:right'>from Didact</h2>
+  </div>
+)
+const container = document.getElementById('root')
+Didact.render(element, container)
